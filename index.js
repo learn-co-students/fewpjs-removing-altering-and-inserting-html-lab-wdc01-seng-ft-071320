16e30bfae398223a6e72563ac45c84b4e39089a3
chsohn15 <chsohn15@gmail.com>
2020-08-23 11:52:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector("main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+
+let head = document.querySelector("head");
+
+document.head.appendChild(newHeader);
+newHeader.innerHTML = "Christina is the champion"
