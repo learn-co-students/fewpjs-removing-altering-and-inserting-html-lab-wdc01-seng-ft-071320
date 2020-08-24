548a0bb48d31f2878cb043391459c8a80e55b111
Abdullah Albasha <Abdullah.a.albasha@gmail.com>
2020-08-24 17:23:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Abdullah is the champion!";
+
+newHeader.className = "victory";
