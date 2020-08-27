1163873775bbb75488410a8f4fcba26b776ccb66
solomonfy <fantahunsolomon@gmail.com>
2020-08-27 03:02:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let main = document.querySelector("main#main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Solomon is the champion";
+
+// document.head.appendChild(newHeader);
